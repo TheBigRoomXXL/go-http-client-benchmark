1a14864b49f4c1f41875d55f64c09b7a16281e42
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-03-13 15:17:51 +0100

    fix: hardcoded localhost

--- benchmark.sh
+++ benchmark.sh
@@ -11,12 +11,12 @@ for delay in $(seq 100 100 2000); do
         # Configure the HTTP server
         wget "$url/config?delay=$delay&size=$size" -q --no-cache --spider
 
-        echo "HTTP server configured with PID: $SERVER_PID, Delay: $delay, Size: $size"
+        echo 'HTTP server configured with : { delay: $delay, size: $size" }'
         
         for c in $(seq 100 100 1500); do
             echo "  - Running hey with concurrency: $c, Delay: $delay, Size: $size"
             
-            OUTPUT=$(hey -c $c -n 10000 http://localhost:8888)
+            OUTPUT=$(hey -c $c -n 10000 $url)
             
             AVG_LATENCY=$(echo "$OUTPUT" | grep "Average:" | awk '{print $2}')
             REQ_PER_SEC=$(echo "$OUTPUT" | grep "Requests/sec:" | awk '{print $2}')
