48fcbc659d90dd0adfa403b771c965d47da9e7d2
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-03-13 11:01:15 +0100

    feat: adapt benchmark.sh to remote configuration

--- benchmark.sh
+++ benchmark.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
-# Function to clean up and stop the server
-cleanup() {
-    if [[ -n "$SERVER_PID" ]]; then
-        sudo kill $SERVER_PID
-    fi
-    exit 0
-}
-
-# Set trap to call cleanup on script exit or interruption
-trap cleanup EXIT INT TERM
+url=$1
 
 echo "Delay,Size,Concurrency,Average Latency (s),Requests/sec" > results.csv
 
 for delay in $(seq 100 100 2000); do
     for size in $(seq 0 500 1000); do
         
-        # Start the HTTP server
-        sudo ./http-mock $delay $size &
-        SERVER_PID=$!
-        echo "HTTP server started with PID: $SERVER_PID, Delay: $delay, Size: $size"
+        # Configure the HTTP server
+        wget "$url/config?delay=$delay&size=$size" -q --no-cache --spider
+
+        echo "HTTP server configured with PID: $SERVER_PID, Delay: $delay, Size: $size"
         
         for c in $(seq 100 100 1500); do
             echo "  - Running hey with concurrency: $c, Delay: $delay, Size: $size"
@@ -32,10 +23,6 @@ for delay in $(seq 100 100 2000); do
             
             echo "$delay,$size,$c,$AVG_LATENCY,$REQ_PER_SEC" >> results.csv
         done
-
-        # Stop the HTTP server
-        sudo kill $SERVER_PID
-        
     done
 done
 
