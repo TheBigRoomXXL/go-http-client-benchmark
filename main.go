dd50259ec1f37b26eef92f8d508052fb2b399a7e
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-03-13 10:29:38 +0100

    feat: add config endpoint for remote configuration

--- main.go
+++ main.go
@@ -5,10 +5,12 @@ import (
 	http "net/http"
 	"os"
 	"strconv"
+	"sync"
 	"time"
 )
 
 func main() {
+	lock := sync.RWMutex{}
 	delay, err := strconv.Atoi(os.Args[1])
 	if err != nil {
 		panic(err)
@@ -18,12 +20,36 @@ func main() {
 		panic(err)
 	}
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		time.Sleep(time.Duration(delay) * time.Millisecond)
-		body := make([]byte, size*1024)
+		lock.RLock()
+		d := delay
+		s := size
+		lock.RUnlock()
+		time.Sleep(time.Duration(d) * time.Millisecond)
+		body := make([]byte, s*1024)
 		for i := range body {
 			body[i] = 'A'
 		}
 		w.Write(body)
+	})
+
+	http.HandleFunc("/config", func(w http.ResponseWriter, r *http.Request) {
+		newDelay, err := strconv.Atoi(r.URL.Query().Get("delay"))
+		if err != nil {
+			w.WriteHeader(400)
+			w.Write([]byte("Bad Request"))
+			return
+		}
+		newSize, err := strconv.Atoi(r.URL.Query().Get("size"))
+		if err != nil {
+			w.WriteHeader(400)
+			w.Write([]byte("Bad Request"))
+			return
+		}
+		lock.Lock()
+		delay = newDelay
+		size = newSize
+		lock.Unlock()
+		w.Write([]byte("OK"))
 	})
 
 	fmt.Println(http.ListenAndServe(":8888", nil))
