bdd5ded7f394433355e7d6827bb976dfca1aba2e
TheBigRoomXXL <seb.lovergne@gmail.com>
2025-03-28 16:36:40 +0100

    feaT: rewrite `benchmark.sh` to use `tc` to controle network limit during batch-4

--- benchmark.sh
+++ benchmark.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
-set -euo pipefail
+set -euo pipefail # Exit on error, undefined variable, or failed pipe
 
-url=$1
+cleanup() {
+    sudo tc qdisc del dev lo root # Remove the network configuration
+    kill $(jobs -p) # Kill the background process
+}
+trap cleanup EXIT # Ensure cleanup is called on exit
 
-echo "Delay,Size,Concurrency,Average Latency (s),Requests/sec" > results.csv
+./http-mock 0 200 > /dev/null 2>&1 & # Start the server with delay=0 and responseSize=200KB
 
-for delay in $(seq 100 100 1000); do
-    for size in $(seq 0 100 500); do
-        # Configure the HTTP server
-        wget "$url/config?delay=$delay&size=$size" -q --no-cache --spider
 
-        echo "HTTP server configured with : { delay: $delay, size: $size }"
+echo "Delay,Bandwidth,Concurrency,Average Latency (s),Requests/sec" > results.csv
+for delay in $(seq 100 100 500); do
+    echo "- Delay: ${delay}ms"
+    for bandwidth in $(seq 1 1 10); do
+        echo "  - Bandwidth: ${bandwidth}Gbps"
+        sudo tc qdisc add dev lo root tbf rate ${bandwidth}gbit burst 512kb latency ${delay}ms
         
-        for c in $(seq 200 200 1000); do
-            echo "  - Running hey with concurrency: $c, Delay: $delay, Size: $size"
-            
-            OUTPUT=$(hey -c $c -z 10s $url)
-            
+        for concurency in $(seq 200 200 1000); do
+            echo "    - Concurrency: $concurency"
+            OUTPUT=$(hey -c $concurency -z 8s http://localhost:8888)
+            echo "$OUTPUT"
             AVG_LATENCY=$(echo "$OUTPUT" | grep "Average:" | awk '{print $2}')
             REQ_PER_SEC=$(echo "$OUTPUT" | grep "Requests/sec:" | awk '{print $2}')
             
-            echo "$delay,$size,$c,$AVG_LATENCY,$REQ_PER_SEC" >> results.csv
+            echo "$delay,$bandwidth,$concurency,$AVG_LATENCY,$REQ_PER_SEC" >> results.csv
         done
+
+        sudo tc qdisc del dev lo root # Remove the network configuration
+        echo "  - Network configuration removed"
     done
 done
 
 echo "Benchmark completed. Results saved in results.csv"
+
+
+sudo tc qdisc add dev lo root tbf rate 1gbit burst 128kb latency 50ms
